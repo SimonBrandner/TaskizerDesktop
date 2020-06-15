424ba87b62ef88ce4c972da1899073c3e803c015
Šimon Brandner <simon@Simon-s-PC.Home>
2020-06-15 08:30:09 +0200

    Added TaskDatabase model

---
+++ src/app/models/task-database.spec.ts
@@ -0,0 +1,7 @@
+import { TaskDatabase } from './task-database';
+
+describe('TaskDatabase', () => {
+  it('should create an instance', () => {
+    expect(new TaskDatabase()).toBeTruthy();
+  });
+});
