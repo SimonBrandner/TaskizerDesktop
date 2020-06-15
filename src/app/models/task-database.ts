5875f452cb0089fe0e7a12d907953f74c4aaedfc
Šimon Brandner <simon@Simon-s-PC.Home>
2020-06-15 09:05:06 +0200

    Added dataChange variable

--- src/app/models/task-database.ts
+++ src/app/models/task-database.ts
@@ -1,2 +1,11 @@
+import { SelectionModel } from "@angular/cdk/collections";
+import { FlatTreeControl } from "@angular/cdk/tree";
+import { Component, Injectable, ElementRef, ViewChild } from "@angular/core";
+import { MatTreeFlatDataSource, MatTreeFlattener } from "@angular/material/tree";
+import { BehaviorSubject, from } from "rxjs";
+import { TaskNode } from "./task-node";
+
+@Injectable()
 export class TaskDatabase {
+	dataChange = new BehaviorSubject<TaskNode[]>([]);
 }
