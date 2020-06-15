fbb7423199e23a360a817200ee088f3e850271aa
Šimon Brandner <simon@Simon-s-PC.Home>
2020-06-15 13:54:44 +0200

    Removed id property

--- src/app/models/flat-task-node.ts
+++ src/app/models/flat-task-node.ts
@@ -1,3 +1,3 @@
 export class FlatTaskNode {
-	constructor(public id: number, public name: string, public level: number, public expandable: boolean) {}
+	constructor(public name: string, public level: number, public expandable: boolean) {}
 }
