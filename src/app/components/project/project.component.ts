cc0e51666b303cca458d9263a8cd67e9ee453ae3
Šimon Brandner <simon@Simon-s-PC.Home>
2020-06-17 13:08:42 +0200

    Prepared basics for TreeView

--- src/app/components/project/project.component.ts
+++ src/app/components/project/project.component.ts
@@ -1,17 +1,97 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ViewChild, ElementRef } from "@angular/core";
 import { ConfigService } from "../../services/config.service";
 import { ProjectService } from "../../services/project.service";
 import { ActivatedRoute } from "@angular/router";
 import { AlgorithmsService } from "../../services/algorithms.service";
 import { CdkDragDrop, moveItemInArray, transferArrayItem } from "@angular/cdk/drag-drop";
-import { Router } from "@angular/router";
-import { Location } from "@angular/common";
+import { MatTreeFlatDataSource, MatTreeFlattener } from "@angular/material/tree";
+import { FlatTreeControl } from "@angular/cdk/tree";
+import { BehaviorSubject, Observable, of, from } from "rxjs";
+import { FlatTaskNode } from "../../models/flat-task-node";
+import { TaskNode } from "../../models/task-node";
+import { TaskDatabase } from "../../models/task-database";
+import { SelectionModel } from "@angular/cdk/collections";
+
+const TEST_DATA = {
+	name: "New project 0",
+	tasks: [
+		{
+			id: 0,
+			name: "Hello world 0",
+			tasks: [
+				{
+					id: 1,
+					name: "Hello world 1",
+					tasks: [
+						{
+							id: 2,
+							name: "Hello world 2",
+							expanded: true,
+							tasks: [
+								{
+									id: 3,
+									name: "Hello world 3",
+									expanded: false
+								},
+								{
+									id: 8,
+									name: "Hello world 8",
+									expanded: false
+								}
+							]
+						},
+						{
+							id: 9,
+							name: "Hello world 9",
+							tasks: [
+								{
+									id: 10,
+									name: "Hello world 10",
+									expanded: false
+								},
+								{
+									id: 11,
+									name: "Hello world 11"
+								}
+							]
+						}
+					]
+				}
+			]
+		},
+		{
+			id: 4,
+			name: "Hello world 4",
+			tasks: [
+				{
+					id: 5,
+					name: "Hello world 5",
+					tasks: [
+						{
+							id: 6,
+							name: "Hello world 6",
+							tasks: [
+								{
+									id: 7,
+									name: "Hello world 7"
+								}
+							]
+						}
+					]
+				}
+			]
+		}
+	]
+};
 
 @Component({
 	selector: "app-project",
 	templateUrl: "./project.component.html",
 	styleUrls: [
 		"./project.component.css"
+	],
+	providers: [
+		TaskDatabase
 	]
 })
 export class ProjectComponent implements OnInit {
@@ -19,8 +99,19 @@ export class ProjectComponent implements OnInit {
 		route: ActivatedRoute,
 		configService: ConfigService,
 		projectService: ProjectService,
-		algorithmsService: AlgorithmsService
+		algorithmsService: AlgorithmsService,
+		private database: TaskDatabase
 	) {
+		this.treeFlattener = new MatTreeFlattener(this.transformer, this.getLevel, this.isExpandable, this.getChildren);
+		this.treeControl = new FlatTreeControl<FlatTaskNode>(this.getLevel, this.isExpandable);
+		this.dataSource = new MatTreeFlatDataSource(this.treeControl, this.treeFlattener);
+
+		database.dataChange.subscribe((data) => {
+			this.dataSource.data = [];
+			this.dataSource.data = data;
+		});
+
+		/*
 		route.params.subscribe((params) => {
 			console.log("Switched to project with id: " + params["id"]);
 			this.projectId = params["id"];
@@ -36,29 +127,48 @@ export class ProjectComponent implements OnInit {
 				});
 			});
 		});
+		*/
 	}
 
-	drop(event: CdkDragDrop<string[]>): void {
-		console.log(event);
+	transformer = (task: TaskNode, level: number) => {
+		const existingNode = this.nestedTaskMap.get(task);
+		const flatNode = existingNode && existingNode.name === task.name ? existingNode : new FlatTaskNode();
+		flatNode.name = task.name;
+		flatNode.level = level;
+		flatNode.expandable = task.tasks && task.tasks.length > 0;
+		this.flatTaskMap.set(flatNode, task);
+		this.nestedTaskMap.set(task, flatNode);
+		return flatNode;
+	};
 
-		if (event.previousContainer === event.container) {
-			moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
-		}
-		else {
-			transferArrayItem(
-				event.previousContainer.data,
-				event.container.data,
-				event.previousIndex,
-				event.currentIndex
-			);
-		}
-	}
+	getLevel = (flatTaskNode: FlatTaskNode) => flatTaskNode.level;
+	isExpandable = (flatTaskNode: FlatTaskNode) => flatTaskNode.expandable;
+	getChildren = (taskNode: TaskNode): Observable<TaskNode[]> => of(taskNode.tasks);
+	hasChild = (_: number, flatTaskNode: FlatTaskNode) => flatTaskNode.expandable;
+	hasNoContent = (_: number, _nodeData: FlatTaskNode) => _nodeData.name === "";
 
 	ngOnInit(): void {}
+
+	flatTaskMap = new Map<FlatTaskNode, TaskNode>();
+	nestedTaskMap = new Map<TaskNode, FlatTaskNode>();
+	selectedParent: FlatTaskNode | null = null;
+	newItemName = "";
+
+	treeControl: FlatTreeControl<FlatTaskNode>;
+	treeFlattener: MatTreeFlattener<TaskNode, FlatTaskNode>;
+	dataSource: MatTreeFlatDataSource<TaskNode, FlatTaskNode>;
+
+	listSelection = new SelectionModel<FlatTaskNode>(true);
+
+	dragTask: any;
+	dragNodeExpandOverWaitTimeMs = 300;
+	dragNodeExpandOverNode: any;
+	dragNodeExpandOverTime: number;
+	dragNodeExpandOverArea: number;
+
+	@ViewChild("emptyItem") emptyItem: ElementRef;
 
 	project: any;
 	projectId: number;
 	projectPath: string;
-	taskIds: String[];
-	topLevelId: String = "-1";
 }
