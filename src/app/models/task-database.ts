a152483270b37bb647643f2c65d0e1dfc76d9312
Šimon Brandner <simon@Simon-s-PC.Home>
2020-06-17 12:38:38 +0200

    Removed unnecessary imports

--- src/app/models/task-database.ts
+++ src/app/models/task-database.ts
@@ -1,8 +1,5 @@
-import { SelectionModel } from "@angular/cdk/collections";
-import { FlatTreeControl } from "@angular/cdk/tree";
-import { Component, Injectable, ElementRef, ViewChild } from "@angular/core";
-import { MatTreeFlatDataSource, MatTreeFlattener } from "@angular/material/tree";
-import { BehaviorSubject, from } from "rxjs";
+import { Injectable } from "@angular/core";
+import { BehaviorSubject } from "rxjs";
 import { TaskNode } from "./task-node";
 
 @Injectable()
