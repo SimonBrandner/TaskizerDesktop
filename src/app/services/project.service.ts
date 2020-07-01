34ddd42810c4efb4443b60f314b5d69642a7b671
Šimon Brandner <simon@Simon-s-PC.Home>
2020-07-01 10:17:02 +0200

    Added getNameFromFullPath() method

--- src/app/services/project.service.ts
+++ src/app/services/project.service.ts
@@ -22,6 +22,10 @@ export class ProjectService {
 		return fullPath.split("/").slice(0, -1).join("/");
 	}
 
+	getNameFromFullPath(fullPath: string) {
+		return fullPath.split("/")[fullPath.split("/").length - 1].split(".")[0];
+	}
+
 	setProjectContent(projectPath: string, project) {
 		this.ipcRenderer.send("setProject", projectPath, "$.tasks", project);
 	}
