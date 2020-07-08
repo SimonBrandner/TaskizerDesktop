d189d3eea189d193a8d909bafa99300d836d7ab0
Šimon Brandner <simon@Simon-s-Notebook.Home>
2020-07-08 10:53:00 +0200

    Displaying deadlines in ProjectComponent

--- src/app/components/project/project.component.ts
+++ src/app/components/project/project.component.ts
@@ -71,6 +71,10 @@ export class ProjectComponent implements OnInit {
 		return flatTask;
 	};
 
+	addTask(task: TaskNode) {
+		this.database.addTask(task);
+	}
+
 	editTask(task: FlatTaskNode) {
 		console.log("Edit task button clicked.");
 		const dialogRef = this.dialog.open(TaskMenuComponent, {
@@ -89,6 +93,75 @@ export class ProjectComponent implements OnInit {
 
 	deleteTask(task: FlatTaskNode) {
 		this.database.deleteTask(this.flatTaskMap.get(task));
+	}
+
+	generateDateOutput(input: Date): string {
+		console.log("Generating date output.");
+		var weekDays: string[] = [
+			"sunday",
+			"monday",
+			"tuesday",
+			"wednesday",
+			"thursday",
+			"friday",
+			"saturday"
+		];
+		var currentDate: Date = new Date();
+
+		if (input == undefined) {
+			return undefined;
+		}
+		else if (
+			input.getMonth() == currentDate.getMonth() &&
+			input.getFullYear() == currentDate.getFullYear() &&
+			input.getDate() == currentDate.getDate()
+		) {
+			return "today";
+		}
+		else if (
+			new Date(input.getFullYear(), input.getMonth(), input.getDate(), 0, 0, 0, 0).getTime() -
+				new Date(
+					currentDate.getFullYear(),
+					currentDate.getMonth(),
+					currentDate.getDate(),
+					0,
+					0,
+					0,
+					0
+				).getTime() ==
+			1000 * 60 * 60 * 24
+		) {
+			return "tomorrow";
+		}
+		else if (
+			new Date(input.getFullYear(), input.getMonth(), input.getDate(), 0, 0, 0, 0).getTime() -
+				new Date(
+					currentDate.getFullYear(),
+					currentDate.getMonth(),
+					currentDate.getDate(),
+					0,
+					0,
+					0,
+					0
+				).getTime() >
+				0 &&
+			new Date(input.getFullYear(), input.getMonth(), input.getDate(), 0, 0, 0, 0).getTime() -
+				new Date(
+					currentDate.getFullYear(),
+					currentDate.getMonth(),
+					currentDate.getDate(),
+					0,
+					0,
+					0,
+					0
+				).getTime() <
+				1000 * 60 * 60 * 24 * 7
+		) {
+			return weekDays[input.getDay().toString()];
+		}
+		else {
+			return undefined;
+		}
 	}
 
 	taskStatusChanged(task: FlatTaskNode) {
@@ -184,10 +257,6 @@ export class ProjectComponent implements OnInit {
 					return "drop-center";
 			}
 		}
-	}
-
-	addTask(task: TaskNode) {
-		this.database.addTask(task);
 	}
 
 	ngOnInit(): void {
