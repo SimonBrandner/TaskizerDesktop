d92d7290624b8ddbdc576f36424e7b4419b54701
Šimon Brandner <simon@Simon-s-Notebook.Home>
2020-07-13 13:44:18 +0200

    Added deb and rpm afterInstall scripts

---
+++ scripts/debAfterInstall.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+echo "deb https://dl.bintray.com/simonbrandner/simon-s-debian-repo default main" | sudo tee -a /etc/apt/sources.list.d/simon-s-debian-repo.list
---
+++ scripts/rpmAfterInstall.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+wget https://bintray.com/simonbrandner/simon-s-rpm-repo/rpm -O bintray-simonbrandner-simon-s-rpm-repo.repo
+sudo mv bintray-simonbrandner-simon-s-rpm-repo.repo /etc/yum.repos.d/
