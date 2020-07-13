bbba9599a1d4c799a8f1df57520065a19136969c
Šimon Brandner <simon@Simon-s-Notebook.Home>
2020-07-13 18:58:00 +0200

    Removed afterInstall scripts

--- scripts/debAfterInstall.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-echo "deb https://dl.bintray.com/simonbrandner/simon-s-debian-repo default main" | sudo tee -a /etc/apt/sources.list.d/simon-s-debian-repo.list
--- scripts/rpmAfterInstall.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-wget https://bintray.com/simonbrandner/simon-s-rpm-repo/rpm -O bintray-simonbrandner-simon-s-rpm-repo.repo
-sudo mv bintray-simonbrandner-simon-s-rpm-repo.repo /etc/yum.repos.d/
