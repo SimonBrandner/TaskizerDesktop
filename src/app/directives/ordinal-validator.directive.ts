60570884912d41c311d452a2f28ece416f2456e7
Šimon Brandner <simon@Simon-s-Notebook.Home>
2020-07-24 10:21:45 +0200

    Removed logging

--- src/app/directives/ordinal-validator.directive.ts
+++ src/app/directives/ordinal-validator.directive.ts
@@ -13,8 +13,6 @@ import { Validator, AbstractControl, NG_VALIDATORS } from "@angular/forms";
 })
 export class OrdinalValidatorDirective implements Validator {
 	validate(control: AbstractControl): { [key: string]: any } | null {
-		console.log(control.value);
-		console.log(control.value < 1);
 		if (control.value != undefined && control.value < 1) {
 			return { ordinalTooSmall: true };
 		}
