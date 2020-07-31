6b2acb94324450c0111e8ff7a007cd28029ca51f
Šimon Brandner <simon.bra.ag@gmail.com>
2020-07-31 11:18:01 +0200

    Moved generateDateOutput() to FlatTaskNode (getDateOutput())

--- src/app/models/flat-task-node.ts
+++ src/app/models/flat-task-node.ts
@@ -10,4 +10,72 @@ export class FlatTaskNode {
 		unit: number[];
 		category: string;
 	};
+
+	getDateOutput() {
+		var weekDays: string[] = [
+			"sunday",
+			"monday",
+			"tuesday",
+			"wednesday",
+			"thursday",
+			"friday",
+			"saturday"
+		];
+		var currentDate: Date = new Date();
+
+		if (this.date == undefined) {
+			return "empty";
+		}
+		else if (
+			this.date.getMonth() == currentDate.getMonth() &&
+			this.date.getFullYear() == currentDate.getFullYear() &&
+			this.date.getDate() == currentDate.getDate()
+		) {
+			return "today";
+		}
+		else if (
+			new Date(this.date.getFullYear(), this.date.getMonth(), this.date.getDate(), 0, 0, 0, 0).getTime() -
+				new Date(
+					currentDate.getFullYear(),
+					currentDate.getMonth(),
+					currentDate.getDate(),
+					0,
+					0,
+					0,
+					0
+				).getTime() ==
+			1000 * 60 * 60 * 24
+		) {
+			return "tomorrow";
+		}
+		else if (
+			new Date(this.date.getFullYear(), this.date.getMonth(), this.date.getDate(), 0, 0, 0, 0).getTime() -
+				new Date(
+					currentDate.getFullYear(),
+					currentDate.getMonth(),
+					currentDate.getDate(),
+					0,
+					0,
+					0,
+					0
+				).getTime() >
+				0 &&
+			new Date(this.date.getFullYear(), this.date.getMonth(), this.date.getDate(), 0, 0, 0, 0).getTime() -
+				new Date(
+					currentDate.getFullYear(),
+					currentDate.getMonth(),
+					currentDate.getDate(),
+					0,
+					0,
+					0,
+					0
+				).getTime() <
+				1000 * 60 * 60 * 24 * 7
+		) {
+			return weekDays[this.date.getDay().toString()];
+		}
+		else {
+			return "default";
+		}
+	}
 }
