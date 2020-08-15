09d70b28acb39f8a87e7228682c34a5c9ea6c8b4
Šimon Brandner <simon.bra.ag@gmail.com>
2020-08-15 12:13:40 +0200

    Added new methods

--- src/app/services/project.service.ts
+++ src/app/services/project.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from "@angular/core";
 import { IpcRenderer } from "electron";
+import { TaskNode } from "../classes/task-node";
 
 @Injectable({
 	providedIn: "root"
@@ -65,6 +66,55 @@ export class ProjectService {
 	deleteProject(projectPath: string): void {
 		console.log("Deleting project with path ", projectPath);
 		this.ipcRenderer.send("deleteProjectFile", projectPath);
+	}
+
+	editTaskByProjectPathAndTaskId(projectPath: string, task: TaskNode): void {
+		console.log("Editing task", task, "from project with path", projectPath);
+
+		this.getProjectByPath(projectPath).then((result) => {
+			this.setProjectContent(projectPath, this.editTaskInProject(result.tasks, task));
+		});
+	}
+
+	private editTaskInProject(oldProjectTasks: Array<TaskNode>, newTask: TaskNode): Array<TaskNode> {
+		var newProjectTasks: Array<TaskNode> = [];
+
+		for (let oldProjectTask of oldProjectTasks) {
+			if (oldProjectTask.id == newTask.id) {
+				oldProjectTask = newTask;
+			}
+			else if (oldProjectTask.tasks.length > 0) {
+				oldProjectTask.tasks = this.editTaskInProject(oldProjectTask.tasks, newTask);
+			}
+			newProjectTasks.push(oldProjectTask);
+		}
+
+		return newProjectTasks;
+	}
+
+	deleteTaskByProjectPathAndTaskId(projectPath: string, taskId: number): void {
+		console.log("Deleting task with id", taskId, "from project with path", projectPath);
+
+		this.getProjectByPath(projectPath).then((result) => {
+			this.setProjectContent(projectPath, this.deleteTaskInProject(result.tasks, taskId));
+		});
+	}
+
+	private deleteTaskInProject(oldProjectTasks: Array<TaskNode>, taskId: number): Array<TaskNode> {
+		var newProjectTasks: Array<TaskNode> = [];
+
+		for (let oldProjectTask of oldProjectTasks) {
+			if (oldProjectTask.id == taskId) {
+			}
+			else {
+				if (oldProjectTask.tasks.length > 0) {
+					oldProjectTask.tasks = this.deleteTaskInProject(oldProjectTask.tasks, taskId);
+				}
+				newProjectTasks.push(oldProjectTask);
+			}
+		}
+
+		return newProjectTasks;
 	}
 
 	async getProjectByPath(projectPath: string): Promise<any> {
